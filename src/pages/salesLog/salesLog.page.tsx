e06d4b4fe8285d86084117f1fbe3bcb09610a708
“AngelaLuu” <angelaht2601@gmail.com>
2025-01-06 21:17:51 -0500

    tabla sales

--- src/pages/salesLog/salesLog.page.tsx
+++ src/pages/salesLog/salesLog.page.tsx
@@ -1,76 +1,80 @@
 
 
 
-const SalesLogPage: React.FC = () => {
-    return (
+const SalesLogPage = () => {
+	return (
 
 
-        <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            Product name
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Color
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Category
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Price
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
-                        </th>
-                        <td className="px-6 py-4">
-                            Silver
-                        </td>
-                        <td className="px-6 py-4">
-                            Laptop
-                        </td>
-                        <td className="px-6 py-4">
-                            $2999
-                        </td>
-                    </tr>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Microsoft Surface Pro
-                        </th>
-                        <td className="px-6 py-4">
-                            White
-                        </td>
-                        <td className="px-6 py-4">
-                            Laptop PC
-                        </td>
-                        <td className="px-6 py-4">
-                            $1999
-                        </td>
-                    </tr>
-                    <tr className="bg-white dark:bg-gray-800">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Magic Mouse 2
-                        </th>
-                        <td className="px-6 py-4">
-                            Black
-                        </td>
-                        <td className="px-6 py-4">
-                            Accessories
-                        </td>
-                        <td className="px-6 py-4">
-                            $99
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+		<div className="relative overflow-x-auto">
+			<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+				<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+					<tr>
+						<th scope="col" className="px-6 py-3">
+							id
+						</th>
+						<th scope="col" className="px-6 py-3">
+							Descripcion
+						</th>
+						<th scope="col" className="px-6 py-3">
+							Precio
+						</th>
+						<th scope="col" className="px-6 py-3">
+							Cantidad
+						</th>
+						<th scope="col" className="px-6 py-3">
+							Precion final
+						</th>
+						<th scope="col" className="px-6 py-3">
+							Fecha de registro
+						</th>
+						<th scope="col" className="px-6 py-3">
+							Registrante
+						</th>
+						<th scope="col" className="px-6 py-3">
+							Metodo de pago
+						</th>
 
-    )
+					</tr>
+				</thead>
+				<tbody>
+					<RowComponent></RowComponent>
+
+				</tbody>
+			</table>
+		</div>
+
+	)
+}
+
+const RowComponent = () => {
+	return (
+		<tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+			<td className="px-6 py-4">
+			2
+			</td>
+			<td className="px-6 py-4">
+			Camiseta
+			</td>
+			<td className="px-6 py-4">
+			45.000
+			</td>
+			<td className="px-6 py-4">
+			2
+			</td>
+			<td className="px-6 py-4">
+			90.000
+			</td>
+			<td className="px-6 py-4">
+			1/6/2025
+			</td>
+			<td className="px-6 py-4">
+			Angela
+			</td>
+			<td className="px-6 py-4">
+			Efectivo
+			</td>
+		</tr>
+	)
 }
 
 export default SalesLogPage
