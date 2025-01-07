b9c160b4b2ede69edd4b9fbea9e2657d05234541
Harold Camargo <hscamargo2002@gmail.com>
2025-01-07 01:02:29 -0500

    🚀  deploy config

--- src/router/config/publicRoute.component.tsx
+++ src/router/config/publicRoute.component.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import ROUTES from '../router.settings';
 import useToken from '../../hooks/useToken.hook';
--- src/router/router.component.tsx
+++ src/router/router.component.tsx
@@ -1,6 +1,6 @@
 //@LIBRERIAS
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 //@SETTINGS
 import ROUTES from './router.settings';
