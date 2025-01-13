65490690d17970870c7e1d8fc4eb2088903b986d
Harold Camargo <hscamargo2002@gmail.com>
2025-01-12 23:02:54 -0500

    borde

--- src/pages/paymentQR/paymentQR.page.tsx
+++ src/pages/paymentQR/paymentQR.page.tsx
@@ -131,7 +131,7 @@ const PaymentQRPage = () => {
 				<SpinnerComponent />
 			</> :
 				<button type="submit" className={` text-white bg-gradient-to-r 
-				from-blue-600 to-red-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2`}>
+				from-blue-600 to-red-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-red-800 font-medium rounded-2xl text-sm px-5 py-2.5 text-center me-2 mb-2`}>
 					Generar QR de pago BOLD
 				</button>
 			}
